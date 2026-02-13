b0accbfec201342ad512f88edb188ce1ad5da3fc
Yi Zhou <61367785+JoeyJo233@users.noreply.github.com>
2026-02-13 15:25:06 +0100

    Chore: change hello.py output to Hello Sweden

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-Simple Hello World script for testing.
+Simple Hello Sweden script for testing.
 """
 
 def main():
-    """Print Hello World to stdout."""
-    print("Hello World")
+    """Print Hello Sweden to stdout."""
+    print("Hello Sweden")
 
 if __name__ == "__main__":
     main()
